93637d0892ceb7db992cf21219ca040c7eeaa403
Sean Barag <sjbarag@gmail.com>
2012-11-10 00:12:44 -0500

    Initial commit
    Seriously, it's just a blank Android project.

---
+++ src/com/designs_1393/textspansion2/textspansion2.java
@@ -0,0 +1,15 @@
+package com.designs_1393.textspansion2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class textspansion2 extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
