12fdd0d7cde9f0e3aa82a9613f569e67555f82a0
Sean Barag <sjbarag@gmail.com>
2012-11-10 01:37:02 -0500

    Isolates and prints last word modified

--- src/com/designs_1393/textspansion2/TextspansionService.java
+++ src/com/designs_1393/textspansion2/TextspansionService.java
@@ -22,12 +22,20 @@ public class TextspansionService extends AccessibilityService
 		String firstText = allTexts.get(0).toString();
 
 		int fromIndex = event.getFromIndex();
+		int addedCount = event.getAddedCount();
 
 		Log.i(TAG, "Text = \"" +firstText +"\"");
-		Log.i(TAG, "fromIndex = " +fromIndex);
+		Log.i(TAG, "fromIndex  = " +fromIndex);
+		Log.i(TAG, "addedCount = " +addedCount);
+
+		String lastInsertion = firstText.substring(fromIndex, fromIndex + addedCount);
+		Log.i(TAG, "Last word  = \"" +lastInsertion +"\"");
+
 
 		if( firstText.charAt(fromIndex) == ' ' )
 			Log.i(TAG, "THAT WAS A SPACE");
+
+		Log.i(TAG, "-----\n");
 	}
 
 	@Override
